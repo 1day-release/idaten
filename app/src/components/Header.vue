031f9ca918efc49d06aa81f53c1494a6ddecacc9
Passionate Engineer Ryoju <ryoju.builder@gmail.com>
2019-01-26 12:51:54 +0900

    Add vue files

---
+++ app/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    header
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+  components: {
+  },
+  props: {
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
