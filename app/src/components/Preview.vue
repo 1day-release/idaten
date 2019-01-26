e26b4aedaab1c58303bd4277aec6cd7bfd59600a
Passionate Engineer Ryoju <ryoju.builder@gmail.com>
2019-01-26 18:14:23 +0900

    slideの基礎の機能を追加

--- app/src/components/Preview.vue
+++ app/src/components/Preview.vue
@@ -1,16 +1,33 @@
 <template>
   <div>
-    preview
+    <Slide class="slide" v-for="(value, index) in slideMarkdown" :key="index" :markdown="value" />
   </div>
 </template>
 
 <script>
+import Slide from '@/components/Slide.vue'
+
 export default {
   name: 'Preview',
+  components: {
+    Slide
+  },
   props: {
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+    slideMarkdown () {
+      return this.$_getSlideMarkdown(this.$store.getters.markdown)
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
+.slide {
+  border: 1px solid gray;
+}
 </style>
