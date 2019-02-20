9e1e13879e21037de2f6cf48f5f4528537073f67
KoheiMayama <mym.kohei@gmail.com>
2019-02-21 00:00:39 +0900

    変更

--- server/api/slides-create/main.go
+++ server/api/slides-create/main.go
@@ -27,9 +27,9 @@ func getNowTime() string {
 func getSlideID(n int) string {
 	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 	const (
-		letterIdxBits = 6                    // 6 bits to represent a letter index
-		letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-		letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+		letterIdxBits = 6
+		letterIdxMask = 1<<letterIdxBits - 1
+		letterIdxMax  = 63 / letterIdxBits
 	)
 
 	var src = rand.NewSource(time.Now().UnixNano())
