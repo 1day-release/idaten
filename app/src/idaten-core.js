d00b74ea4182dae8fd6e45b6bf7f9961063ede2e
idaten-core CircleCI <admin@1day-release.com>
2019-04-09 09:46:25 +0000

    Deploy idaten-core by CircleCI

--- app/src/idaten-core.js
+++ app/src/idaten-core.js
@@ -6,4 +6,4 @@
 /*!*********************!*\
   !*** ./src/test.ts ***!
   \*********************/
-/*! exports provided: default */function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return test; });\nfunction test() {\n    return 'Hello';\n}\n\n\n//# sourceURL=webpack:///./src/test.ts?")}});
+/*! exports provided: default */function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return test; });\nfunction test() {\n    return 'Hello2';\n}\n\n\n//# sourceURL=webpack:///./src/test.ts?")}});
