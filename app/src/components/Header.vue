9a3e1c545af34b64055012a0f03ef992a2614181
KirinNakayama <n.kirin.0115@gmail.com>
2019-05-09 00:31:32 +0900

    設定系ポップアップコーディング

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -37,9 +37,58 @@
               </div>
               <IconButton balloon-text="スライドをシェア" svg="@/assets/icon-link.svg" />
             </div>
+            <PopupSetting id="popup-setting-share" style="right:-191px;">
+              <aside>
+                <dl class="setting-item">
+                  <dt>ボードの共有設定</dt>
+                  <dd>
+                    <input type="radio" class="input-radio" name="board-share" id="board-share-private" checked />
+                    <label for="board-share-private">
+                      <span>
+                        <strong class="color-text-sub">自分だけが</strong>アクセスできる
+                      </span>
+                    </label>
+                  </dd>
+                  <dd>
+                    <input type="radio" class="input-radio" name="board-share" id="board-share-view" />
+                    <label for="board-share-view">
+                      <span>
+                        リンクを知ってる全員が<strong class="color-text-sub">閲覧できる</strong>
+                      </span>
+                    </label>
+                  </dd>
+                  <dd>
+                    <input type="radio" class="input-radio" name="board-share" id="board-share-edit" />
+                    <label for="board-share-edit">
+                      <span>
+                        リンクを知ってる全員が<strong class="color-text-sub">編集できる</strong>
+                      </span>
+                    </label>
+                  </dd>
+                </dl>
+                <hr />
+                <input type="text" class="input-text" name="" id="" value="ここに今いるURLぶち込んでおくれーここに今いるURLぶち込んでおくれーここに今いるURLぶち込んでおくれー" />
+                <ul class="button-list">
+                  <li><router-link class="submit-button" to="javascript:void(0)">リンクをコピー</router-link></li>
+                </ul>
+              </aside>
+            </PopupSetting>
           </li>
           <li>
             <IconButton balloon-text="設定" svg="@/assets/icon-setting.svg" />
+            <PopupSetting id="popup-setting-slide" style="right:-124px;">
+              <aside>
+                <dl class="setting-item">
+                  <dt>ボードの削除</dt>
+                  <dd>
+                    <ul class="button-list">
+                      <li><router-link class="submit-button is-red" to="javascript:void(0)">このボードを削除する</router-link></li>
+                      <li><router-link class="submit-button is-link" to="javascript:void(0)">キャンセル</router-link></li>
+                    </ul>
+                  </dd>
+                </dl>
+              </aside>
+            </PopupSetting>
           </li>
           <li class="is-separate is-login">
             <IconButton balloon-text="ログアウト" svg="@/assets/icon-logout.svg" />
@@ -63,6 +112,7 @@ import TextButton from '@/components/TextButton.vue'
 import UserIcon from '@/components/UserIcon.vue'
 import UpdateIcon from '@/assets/icon-refresh.svg'
 import ButtonMask from '@/assets/icon-mask.svg'
+import PopupSetting from '@/components/PopupSetting.vue'
 
 export default {
   name: 'Header',
@@ -72,7 +122,8 @@ export default {
     TextButton,
     UserIcon,
     UpdateIcon,
-    ButtonMask
+    ButtonMask,
+    PopupSetting
   },
   props: {
   },
---
+++ app/src/components/PopupSetting.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="popup-setting">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PopupSetting',
+  props: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .popup-setting {
+    $bgColor: #fff;
+
+    box-sizing: border-box;
+    position: absolute;
+    z-index: 10;
+    top: calc(100% + 10px);
+    width: 370px;
+    padding: 20px;
+    background-color: $bgColor;
+    box-shadow: 4px 0 11px 0 rgba(0, 0, 0, 0.3);
+
+    opacity: 0;
+    pointer-events: none;
+
+    &::before,
+    &::after {
+      content: "";
+      position: absolute;
+      background-color: $bgColor;
+      display: block;
+    }
+
+    &::before {
+      bottom: calc(100% - 4px);
+      width: 6px;
+      height: 6px;
+      box-shadow: inherit;
+      transform: rotate(45deg);
+      transform-origin: center center;
+    }
+
+    &::after {
+      top: 0;
+      width: 20px;
+      height: 10px;
+    }
+
+    &.is-active {
+      pointer-events: auto;
+      opacity: 1;
+    }
+
+    .setting-item {
+
+      >dt {
+        margin: 10px 0;
+        font-size: 1.0rem;
+        color: #5a5a5a;
+      }
+
+      >dd {
+        display: block;
+        overflow: hidden;
+        margin-top: 1px;
+      }
+
+      strong {
+        font-weight: bold;
+      }
+    }
+
+    hr {
+      display: block;
+      width: 100%;
+      height: 1px;
+      border: 0;
+      margin: 20px 0 30px;
+      background-color: rgba(#000, 0.1);
+    }
+
+    .button-list {
+      margin: 10px 0;
+
+      >li {
+        margin-top: 20px;
+
+        &:first-child {
+          margin-top: 0;
+        }
+      }
+    }
+  }
+</style>
